df0b008baf150396f570312a767606bcfa7e4cae
Slaven Rezić <slaven@rezic.de>
1999-04-08 22:47:34 +0000

    Import of SREZIC/Tk-TIFF-0.05 from CPAN.
    gitpan-cpan-distribution: Tk-TIFF
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         SREZIC/Tk-TIFF-0.05.tar.gz
    gitpan-cpan-author:       SREZIC
    gitpan-cpan-maturity:     released

--- TIFF.pm
+++ TIFF.pm
@@ -1,13 +1,12 @@
 package Tk::TIFF;
 require DynaLoader;
 use Tk 800.014;
-require Tk::Image;
 require Tk::Photo;
 
 use vars qw($VERSION @ISA);
 @ISA = qw(DynaLoader);
 
-$VERSION = '0.04';
+$VERSION = '0.05';
 
 bootstrap Tk::TIFF $Tk::VERSION;
 
